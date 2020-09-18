c42e25bf727092873b377089c6600efe3b512e55
Jakub Szychulec <jszychulec@gmail.com>
2020-09-18 03:26:31 +0200

    add sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.aside`
+height: 100%;
+width: 250px;
+max-width: calc(100% - 25px);
+box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+background: white;
+position: absolute;
+top: 0;
+left: 0;
+display: flex;
+flex-direction: column;
+
+	@media (max-width: 600px) {
+		box-shadow: ${props => props.expanded && '0px 0px 0px transparent'};
+		transform: ${props => props.expanded && 'translateX(-100%)'}
+	}
+
+`;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from '../../components/GlobalStyle';
+import Sidebar from '../../components/Sidebar';
 
 function App() {
-  return <GlobalStyle>
-    <div></div>
-  </GlobalStyle>;
+  return <>
+    <GlobalStyle />
+    <Sidebar />
+  </>
 }
 
 export default App;
