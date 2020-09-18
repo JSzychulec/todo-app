8f6e17b9a24d2f4e96f69de67336135d7503bdfe
Jakub Szychulec <jszychulec@gmail.com>
2020-09-18 19:13:38 +0200

    fix AppName responsiveness

--- src/components/AppName.js
+++ src/components/AppName.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 
 export default styled.h1`
-	color: ${props => props.expanded ? 'black' : 'white'};
+	color: black;
 	font-size: 20px;
 	font-family: sans-serif;
 	font-weight: 400;
 	position: absolute;
 	z-index: 1;
 	margin: 0;
-	left: 55px;
+	left: 15px;
 	top: 15px;
 	transition: 1s;
+
+	@media (max-width: 600px) {
+		color: ${props => props.expanded ? 'black' : 'white'};
+		left: 55px;
+	}
+
 `
