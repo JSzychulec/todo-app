f756171ddfec681b99dd980a1878fec21540b792
Jakub Szychulec <jszychulec@gmail.com>
2020-09-30 12:28:07 +0200

    create add list button

---
+++ src/components/AddListButton.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Button from './Button';
+
+const AddListButton = ({ className }) => <Button className={className}>add list</Button>
+
+export default styled(AddListButton)`
+position: absolute;
+top: 11px;
+right: 15px;
+transition: opacity 0.3s 1s;
+
+	@media (max-width: ${props => props.theme.breakpoint}) {
+		opacity: ${props => props.expanded ? 1 : 0};
+		pointer-events: ${props => props.expanded ? 'inherit' : 'none'};
+		transition: opacity ${props => props.expanded ? '0.3s 1s' : '0.3s 0s'};
+	}
+`;
---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export default styled.button`
+	border: none;
+	background: ${props => props.theme.brandColor};
+	border: 2px solid transparent;			
+	color: white;
+	padding: 6px;
+	border-radius: 5px;
+	cursor: pointer;
+	font-family: sans-serif;
+	font-size: 12px;
+	letter-spacing: 1px;
+	font-weight: bold;
+	text-transform: uppercase;
+
+	&:hover {
+		background: transparent;
+		border: 2px solid ${props => props.theme.brandColor};
+		color: ${props => props.theme.brandColor};
+	}
+
+`;
