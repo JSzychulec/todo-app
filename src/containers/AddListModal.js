6221eac4e875122c8f7af20a56223eba2aa89a6c
Jakub Szychulec <jszychulec@gmail.com>
2020-10-05 00:29:26 +0200

    add submitting on 'Enter' press to AddListModal

--- src/containers/AddListModal.js
+++ src/containers/AddListModal.js
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 import Button from '../components/Button';
+import FormInput from '../components/Form/FormInput';
+import FormItem from '../components/Form/FormItem';
 import Modal from '../components/Modal';
 import useTodos from '../hooks/useTodos';
-
-const UnstyledFormItem = ({ className, children, id, label, required }) => {
-    return <div className={className}>
-        {label && (<FormItemLabel for={id}>
-            {label}{required && <RequiredIndicator />}
-        </FormItemLabel>)}
-        {children}
-    </div>
-}
-
-const FormItem = styled(UnstyledFormItem)`
-    display: flex;
-    flex-direction: column;
-    align-items: baseline;
-`;
-
-const FormInput = styled.input`
-    padding: 3px;
-    font-size: 15px;
-    margin-bottom: 20px;
-`;
-
-const FormItemLabel = styled.label`
-    font-family: sans-serif;
-    margin-bottom: 5px;
-`;
-
-const UnstyledRequiredIndicator = ({ className }) => <span className={className}>*</span>
-const RequiredIndicator = styled(UnstyledRequiredIndicator)`
-    color: red;
-    margin-left: 5px;
-`;
 
 const AddListModal = ({ close, ...props }) => {
     const { addList } = useTodos();
