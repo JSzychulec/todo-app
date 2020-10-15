b1fd0eb2314f4bb5bf6a0ac7deb2bf970953c0e7
Jakub Szychulec <jszychulec@gmail.com>
2020-10-16 01:55:14 +0200

    expand button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export default styled.button`
 	border: none;
-	background: ${props => props.theme.brandColor};
-	border: 2px solid transparent;			
+	background: ${props => props.red ? '#e74c3c' : props.theme.brandColor};
+	border: 2px solid transparent;
 	color: white;
 	padding: 4px;
 	border-radius: 5px;
 	cursor: pointer;
-	font-family: sans-serif;
+	font-family: sans - serif;
 	font-size: 12px;
 	letter-spacing: 1px;
 	font-weight: bold;
@@ -16,8 +16,8 @@ export default styled.button`
 
 	&:hover {
 		background: transparent;
-		border: 2px solid ${props => props.theme.brandColor};
-		color: ${props => props.theme.brandColor};
+		border: 2px solid ${props => props.red ? '#e74c3c' : props.theme.brandColor};
+		color: ${props => props.red ? '#e74c3c' : props.theme.brandColor};
 	}
 
 `;
